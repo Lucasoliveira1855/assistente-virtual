f611c3013519740e693ea7f224bc36be0b07b443
Lucasoliveira1855 <oliveiralucas85250@gmail.com>
2022-03-31 00:49:35 +0000

    elisa

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import speech_recognition as sr 
+import speech_recognition as sr
+import pyttsx3
 
-#cria um reconhecedor
-r = sr.Recognizer()
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+maquina.say('Ola, eu sou a Elisabeth')
+maquina.say('Como posso ajudar-lo?')
+maquina.runAndWait()
 
-#abrir o microfone para captura
-with sr.Microphone() as source:
-    while True:
-     audio = r.listen(source) #define microfone como fonte de audio
+try:
+    with sr.Microphone() as source:
+        print('Ouvindo...')
+        voz = audio.listen(source)
+        comando = audio.recognize_google(voz, language='pt-BR')
+        comando = comando.lower()
+        if 'Elisabeth' in comando:
+            print(comando)
 
-print(r.recognize_google(audio, language='pt'))
+except:
+    print('Microfone nao esta ok')
